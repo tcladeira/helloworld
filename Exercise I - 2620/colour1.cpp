d0af7fdec8eec9812ad45a91c4b2e7f2966c53c8
Thiago <tcladeira@gmail.com>
2023-07-25 18:23:57 -0600

    Structure of the functions

--- Exercise I - 2620/colour1.cpp
+++ Exercise I - 2620/colour1.cpp
@@ -3,8 +3,31 @@ using namespace std;
 
 
 
+void sort( string A[], int n){
+
+}
 
 
+bool linear_search (const string A[], int n, string colour, int &count){
+    
+    
+    return;
+}
+bool binary_search (const string A[],int n, string colour, int &count){
+    
+    
+    return;
+}
+string *resize (string *A, int n){
+    
+    
+    return;
+}
+void search_and_report (const string A[], int n, string colour, string label, 
+            bool (*search) (const string A[], int n, string colour, int &count)){
+    
+    
+    }    
 
 
 
